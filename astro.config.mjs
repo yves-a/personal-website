ab3736e8dd4cc0d289ac34dc9311ca9bf411ceb4
Yves <yalikalfic@yahoo.com>
2026-01-17 14:38:43 -0500

    update base href

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Replace 'username' and 'repo-name' with your actual info
   site: 'https://yves-a.github.io',
-  base: '/personal-website/',
+  base: '/personal-website',
+  trailingSlash: 'always',
 });
